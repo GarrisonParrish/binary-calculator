d922daa98fea5bc45ee5dc6be974e1ca0f0f805f
Garrison Parrish <tpgarris@live.unc.edu>
2021-09-16 00:48:23 -0400

    Started biniary operations

---
+++ binary_operations.py
@@ -0,0 +1,5 @@
+"""Operations on binary numbers."""
+
+def bin_addition():
+    """Add two binary numbers (as lists) together and return the result as a list of integers."""
+    # Which is faster, actually performing the calculation on the binary number or just adding the decimals and converting that result to binary?
