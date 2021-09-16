cfb7ffcf64f988a3c157618f3b9748fe4d791732
Garrison Parrish <tpgarris@live.unc.edu>
2021-09-16 00:44:33 -0400

    Cleaned up the mess of files

--- binary_conversions.py
+++ binary_conversions.py
@@ -1,22 +1,17 @@
 """Handle conversions from binary (as lists) to other forms."""
 
 
-
 def bin_to_dec(bin: str) -> int:
-    """Convert binary string to decimal integer."""
-    bin_len: int = len(bin)
-
+    """Convert binary number (as list) to decimal integer."""
     # Take string and parse it from right to left into an array of ints
     # All extra ints are left as 0's (do I need this step?)
     # Feed this array into the algorithm
     # Starting at rightmost bit ( i = N-1 ), add 2^(i) to the decimal result
     # When finished with the array, should have the result
-
      # probably not the most efficient way to do this but it counts down from the top and it works
-
     dec_result: int = 0
     j: int = 0  # counter for exponents (there has to be a better way to do this)
-    for i in range(bin_len-1, -1, -1):
+    for i in range(len(bin)-1, -1, -1):
         if bin[i] != '1' and bin[i] != '0':
             print("Invalid bit string: must contain only 1's or 0's")
             return -1  # NOTE: may need to find a better way to handle this error
@@ -57,4 +52,17 @@ def bin_to_ones_compl(bin, neg: bool):
             else:
                 bin[bit] = 1
     
+    return bin
+
+
+def bin_to_twos_compl(bin):
+    """Converts binary (as array) to twos complement negative."""
+    for bit in range(0, len(bin)):
+        # Flip the bits
+        if bin[bit] == 1:
+            bin[bit] = 0
+        else:
+            bin[bit] = 1
+
+    # Add 1
     return bin
--- decimal_conversions.py
+++ decimal_conversions.py
@@ -3,26 +3,6 @@
 
 # NOTE: A lot of this code is just plain bad
 
-def dec_to_ones_compl(dec):
-    """Converts a decimal to signed ones complement."""
-    # NOTE: method overloading is not possible in Python
-
-    # If dec is negative (below 0):
-    # Take absolute value
-    # Convert to unsigned magnitude binary
-    # Convert to ones complement with neg = True
-    
-    # If dec is positive:
-    # Convert to unsigned magnitude binary
-    # Convert to ones complement with neg = False (changes nothing)
-
-    neg: bool = False
-    if dec < 0:
-        neg = True
-        dec = abs(dec)
-    
-    return bin_to_ones_compl(dec_to_bin(dec, 8), neg)
-
 
 def dec_to_bin(dec: int, N: int = 32):
     """Converts decimal integer to N-bit unsigned binary as a list. N defaults to 32 bits."""
@@ -51,3 +31,25 @@ def dec_to_bin(dec: int, N: int = 32):
         i -= 1
     
     return bits
+
+
+def dec_to_ones_compl(dec):
+    """Converts a decimal to signed ones complement."""
+    # NOTE: this one is broken
+
+    # If dec is negative (below 0):
+    # Take absolute value
+    # Convert to unsigned magnitude binary
+    # Convert to ones complement with neg = True
+    
+    # If dec is positive:
+    # Convert to unsigned magnitude binary
+    # Convert to ones complement with neg = False (changes nothing)
+
+    neg: bool = False
+    if dec < 0:
+        neg = True
+        dec = abs(dec)
+    
+    # return bin_to_ones_compl(dec_to_bin(dec, 8), neg)
+    return 0
